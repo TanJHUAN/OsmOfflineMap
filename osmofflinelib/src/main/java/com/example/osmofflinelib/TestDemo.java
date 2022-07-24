6199fe7cfeba4d11761d95785ce204be48a66bad
TanJHUAN <1250636143@qq.com>
2022-07-24 19:19:08 +0800

    Initial commit

---
+++ osmofflinelib/src/main/java/com/example/osmofflinelib/TestDemo.java
@@ -0,0 +1,10 @@
+package com.example.osmofflinelib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class TestDemo {
+    public static void showToast(Context context){
+        Toast.makeText(context, "成功！", Toast.LENGTH_SHORT).show();
+    }
+}
